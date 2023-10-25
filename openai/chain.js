d8624747ac178ea007ea1d1e9d575d75792c4386
Rafael Ferres <raferres@outlook.com.br>
2023-10-25 19:44:53 -0300

    Add comments

--- openai/chain.js
+++ openai/chain.js
@@ -5,16 +5,21 @@ const { OpenAI } = require("langchain/llms/openai");
 const { PineconeStore } = require("langchain/vectorstores/pinecone");
 
 const callChain = async (message) => {
+  // Instancia o client do pinecone
   const pinecone = new Pinecone();
 
+  // Define o index do pinecone
   const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX);
 
+  // Define o VectorStore para pesquisarmos os dados
   const vectorStore = await PineconeStore.fromExistingIndex(
     new OpenAIEmbeddings(),
     { pineconeIndex }
   );
 
+  // Instancia o modelo que iremos utilizar, aqui podemos configurar o todas as opções de modelo como temperatura, qual modelos iremos utilizar gpt-4 ou gpt-3.5-turbo e afins
   const model = new OpenAI();
+
   const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
     k: 1,
     returnSourceDocuments: true,
--- pinecone/save.js
+++ pinecone/save.js
@@ -7,12 +7,16 @@ const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
  * @param {*} output documentos
  */
 const savePinecone = async (output) => {
+  // Instancia o client do pinecone
   const client = new Pinecone();
 
+  // Define o index do pinecone
   const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
 
+  // Instancia a clase de Embeddings da OpenAI
   const embeddings = new OpenAIEmbeddings();
 
+  // Gera os embeddings e após salva no pinecone no index que foi informado
   await PineconeStore.fromDocuments(output, embeddings, {
     pineconeIndex,
   });
